b5ae405bdf16a4bd58341583faf3c6cc9adbaa30
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-02-23 12:53:48 -0800

    set teamMembers & formValues STATE

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import React, {useState} from "react"
 import Form from './components/Form'
 import TeamMembersList from './components/TeamMembersList'
 
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+}
 
 function App() {
+
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue
+    })
+  }
+
+  const submitForm = () => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role.trim()
+    }
+    if(!newTeamMember.name || !newTeamMember.email || !newTeamMember.role){
+      return;
+    }
+    setFormValues(initialFormValues)
+  }
+
   return (
     <div className="App">
       <h1>Team Builder</h1>
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,17 +1,29 @@
 import React from "react"
 
 const Form = () => {
+
+    
+
+
     return (
         <div>
             <h2>Add A Team Member</h2>
             <form>
                 <label>
                 Name:
-                    <input type="text" name="name" />
+                    <input 
+                        type="text"
+                        name="name"
+                        autoComplete="off"
+                    />
                 </label><br />
                 <label>
                 Email:
-                <input type="email" name="email" />
+                <input 
+                    type="email" 
+                    name="email" 
+                    autoComplete="off"
+                />
                 </label><br />
                 <select>
                     <option>-- Select Role --</option>
